0c946d9c7f1d7579fe90d77e0e4d1260d295854b
MartianKnight <MartianCastle@gmail.com>
2014-12-06 08:29:40 -0500

    Frame for js and css

--- prototype/stage1/main.js
+++ prototype/stage1/main.js
@@ -1,1 +1,93 @@
-/* Stage 1 JS */
+/**
+ * Stage 1 JS
+ */
+
+/**
+ *  JS for Animations Object
+ *
+ *  This function will provide all the functions for SVGs
+ *
+ */
+function Animations() {
+  this.x = 0;
+  this.y = 0;
+  this.scale = 0
+  this.
+}
+
+/**
+ *  JS for Volcanoes
+ *
+ *  Setup for the volcano svg
+ *
+ */
+Animations.prototype.volcano function() {
+
+}
+
+/**
+ *  JS for Tornado
+ *
+ *  Setup for the tornado svg
+ *
+ */
+Animations.prototype.tornado function() {
+
+}
+
+/**
+ *  JS for Hurricane
+ *
+ *  Setup for the hurricane svg
+ *
+ */
+Animations.prototype.hurricane function() {
+
+}
+
+/**
+ *  JS for Earthquake
+ *
+ *  Setup for the earthquake svg
+ *
+ */
+Animations.prototype.earthquake function() {
+
+}
+
+/**
+ *  JS for Help Animations
+ *
+ *  This function will provide animations at the beginning of the page load to
+ *  help the user understand what he/she can interact with.
+ *
+ */
+Animation.prototype.help function() {
+
+}
+
+/**
+ *  JS for Information Boxes
+ *
+ *  This function will provide boxes of information to pop up next to the
+ *  SVGs on the page
+ *
+ */
+Animation.prototype.info function() {
+
+}
+
+// Define Objects
+var Charlotte = new Animation();
+
+
+
+/* JS for data listing */
+
+/* JS for disaster scales */
+
+/* JS for svg area */
+
+/* JS for top chart area */
+
+/* JS for  */
