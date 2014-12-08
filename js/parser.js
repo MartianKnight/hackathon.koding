e5f89533bcb95cb2eac9647ac4ab6ee0771b6f55
spider-many <sgirma53@gmail.com>
2014-12-07 21:13:11 -0500

    returns json array

--- js/parser.js
+++ js/parser.js
@@ -80,6 +80,7 @@ getScale();
 function getScale() {
 	var sum = 0;
 	var count = 0;
+	var scaleAvgArray = [];
 
 	//potentialDisasters = fileName.format();//getDisaster(stateName);
 
@@ -104,16 +105,16 @@ function getScale() {
 				//TODO: fix to see if there's a contains function in javascript
 				switch(disaster[disasterNumber].substring(3, disaster[disasterNumber].length - 5)) {
 					case "tornado":
-						console.log(disaster[disasterNumber] + getTornadoCounter(data));
+						scaleAvgArray[0] = disaster[disasterNumber] + getTornadoCounter(data);
 						break;
 					case "hurricane":
-						console.log(disaster[disasterNumber] + getHurricaneCounter(data));
+						scaleAvgArray[1] = disaster[disasterNumber] + getHurricaneCounter(data);
 						break;
 					case "quakes":
-						console.log(disaster[disasterNumber] + getEarthquakeCounter(data));
+						scaleAvgArray[2] = disaster[disasterNumber] + getEarthquakeCounter(data);
 						break;
 					case "volcano":
-						console.log(disaster[disasterNumber] + getVolcanoCounter(data));
+						scaleAvgArray[3] = disaster[disasterNumber] + getVolcanoCounter(data);
 						break;
 					default:
 						console.log("didn't get disaster" + disaster[disasterNumber]);
@@ -134,6 +135,9 @@ function getScale() {
 			});*/
 		})(disasterNumber);
 	}
+
+	console.log("scale avg array: " + scaleAvgArray);
+	return scaleAvgArray;
 }
 
 function getTornadoCounter(tornadoJson){
@@ -169,7 +173,7 @@ function getTornadoCounter(tornadoJson){
 			}
 		}
 	}
-	//console.log(fujitaCounts["Fujita0"]);
+	console.log(fujitaCounts["Fujita0"]);
 	return fujitaCounts;
 }
 
@@ -179,18 +183,30 @@ function getVolcanoCounter(volcanoJson){
 	// times that volcanos of VEI scale 0 appear in the dataset, etc.
 
 	//TODO: Return counts in dictinary/json format
-	var veiCounts = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+	//var veiCounts = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+	var veiCounts = {
+		"vei0":0,
+		"vei1":0,
+		"vei2":0,
+		"vei3":0,
+		"vei4":0,
+		"vei5":0,
+		"vei6":0,
+		"vei7":0,
+		"vei8":0
+	}
 
 	for(volcanoInstance in volcanoJson) {
 
-		for(var veiValue = 0; veiValue <= veiCounts.length; veiValue++) {
+		for(var veiValue = 0; veiValue <= 8; veiValue++) {
 		// loop through all possible VEI Scale values
 
 
 			if(Number(volcanoJson[volcanoInstance].VEI) === veiValue) {
 			// Add the VEI Scale value from this instance to count array
 
-				veiCounts[veiValue]++;
+				//veiCounts[veiValue]++;
+				veiCounts["vei" + veiValue]++;
 			}
 
 		}
@@ -200,35 +216,43 @@ function getVolcanoCounter(volcanoJson){
 }
 
 function getEarthquakeCounter(quakeJson){
-	// The 0th element of the array corresponds to a magnitude of 4-4.9 on
-	// the Richter Scale, etc.
-	// TODO: Check (if statement) that EQ_PRIMARY attribute exists
-	// TODO: Return counts in dictionary/json format
-	// TODO: consider renaming variables
-	// TODO: test
-	var magnitudeCounts = [0, 0, 0, 0, 0];
+	//var magnitudeCounts = [0, 0, 0, 0, 0];
+	var magnitudeCounts = {
+		"magnitude4":0,
+		"magnitude5":0,
+		"magnitude6":0,
+		"magnitude7":0,
+		"magnitude8":0
+	}
 
 	for(quakeInstance in quakeJson) {
-	//for(var quakeId = 0; quakeId <= quakeJson; quakeId++){
-
-		for(var quakeValue = 4; quakeValue <= magnitudeCounts.length + 4; quakeValue++) {
-		// loop through all possible Richter Scale values
-
-			if(Math.floor(Number(quakeJson[quakeInstance].EQ_PRIMARY)) === quakeValue) {
+		for(var quakeValue = 4; quakeValue <= 8; quakeValue++) {
+			if(Math.floor(Number(quakeJson[quakeInstance].EQ_PRIMARY)) == quakeValue) {
 			// Adds the Richter Scale value from this instance to count array
-				magnitudeCounts[quakeValue - 4]++;
+				//magnitudeCounts[quakeValue - 4]++;
+				magnitudeCounts["magnitude" + quakeValue]++;
 			}
 		}
 	}
 
 	return magnitudeCounts;
-
 }
 
 function getHurricaneCounter(hurricaneJson){
 	// initialize category count
 	// TODO: return category counts in dictionary/json format
-	var tropicalLowCount = 0;
+	var categoryCounts = {
+		"tropicalLow":0,
+		"tropicalDepression":0,
+		"tropicalStorm":0,
+		"category1":0,
+		"category2":0,
+		"category3":0,
+		"category4":0,
+		"category5":0,
+		"subTropicalStorm":0,
+	}
+	/*var tropicalLowCount = 0;
 	var tropicalDepressionCount = 0;
 	var tropicalStormCount = 0;
 	var category1Count = 0;
@@ -236,7 +260,7 @@ function getHurricaneCounter(hurricaneJson){
 	var category3Count = 0;
 	var category4Count = 0;
 	var category5Count = 0;
-	var subTropicalStormCount = 0;
+	var subTropicalStormCount = 0;*/
 
 	// count all possible hurricane categories as they appear in the .json files
 	for(hurricane_instance in hurricaneJson) {
@@ -250,36 +274,36 @@ function getHurricaneCounter(hurricaneJson){
 		var classificationMinusSpace = hurricaneJson[hurricane_instance][Max_Classification].replace(/\s+/g, '');
 		switch(classificationMinusSpace) {//hurricaneJson[hurricane_instance].Max_Classification) {
 			case "TropicalLow":
-				tropicalLowCount++;
+				categoryCounts["tropicalLow"]++;
 				break;
 			case "TropicalStorm":
-				tropicalStormCount++;
+				categoryCounts["tropicalStorm"]++;
 				break;
 			case "TropicalDepression":
-				tropicalDepressionCount++;
+				categoryCounts["tropicalDepression"]++;
 				break;
 			case "SubTropicalStorm":
-				subTropicalStormCount++;
+				categoryCounts["subTropicalStorm"]++;
 				break;
 			case "Category1":
-				category1Count++;
+				categoryCounts["category1"]++;
 				break;
 			case "Category2":
-				category2Count++;
+				categoryCounts["category2"]++;
 				break;
 			case "Category3":
-				category3Count++;
+				categoryCounts["category3"]++;
 				break;
 			case "Category4":
-				category4Count++;
+				categoryCounts["category4"]++;
 				break;
 			case "Category5":
-				category5Count++;
+				categoryCounts["category5"]++;
 				break;
 			default:
 				console.log("ERROR: Category " + hurricaneJson[hurricane_instance].Max_Classification + " not expected.");
 			}
 		}
 
-	return [tropicalLowCount, subTropicalStormCount, tropicalStormCount, tropicalDepressionCount, category1Count, category2Count, category3Count, category4Count, category5Count];
+	return categoryCounts;// [tropicalLowCount, subTropicalStormCount, tropicalStormCount, tropicalDepressionCount, category1Count, category2Count, category3Count, category4Count, category5Count];
 }
